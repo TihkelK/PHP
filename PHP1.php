8a10c1a1f49a9f1ee7bd1e85c47f163c3615d2b9
TihkelK <mihkel.kuusemae@hkhk.edu.ee>
2024-02-01 11:23:57 +0200

    AAA

---
+++ PHP1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Harjutus 1</h1>   
+
+        <?php
+        /*
+        // Ülesanne 1
+        // Mihkel Kuusemäe
+        // 01.01.24
+        */
+
+        $nimi = "Mihkel";
+        $synniaasta = 2006;
+        $tahtkuju = "libra";
+
+        echo "Minu nimi on $nimi.<br>Sünniaasta - $synniaasta<br>Tähemärk - $tahtkuju <br><br>";
+
+        echo "\"It's My Life\" – Dr. Alban<br><br>";
+
+        echo "(\(\ <br>";
+        echo "( -.-) <br>";
+        echo "o_(\")(\") <br>";
+
+
+
+        ?>
+
+    </div>
+
+</body>
+</html>
---
+++ PHP2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Harjutus 2</h1>   
+
+        <?php
+        /*
+        // Ülesanne 1
+        // Mihkel Kuusemäe
+        // 01.01.24
+        */
+
+        $m1 = 2;
+        $m2 = 5;
+
+        $sum = $m1 + $m2;
+        $lah = $m1 - $m2;
+        $kor = $m1 * $m2;
+        $jag = $m1 / $m2;
+        $jaak = $m1 % $m2;
+        
+        echo "Liitmine = $sum tehe ($m1 + $m2)<br>";
+        echo "Lahutamine = $lah tehe ($m1 - $m2)<br>";
+        echo "Korrutamine = $kor tehe ($m1 * $m2)<br>";
+        echo "Jagamine = $jag tehe ($m1 / $m2)<br>";
+        echo "Jääk = $jaak tehe ($m1 % $m2)<br>";
+        
+        $mm = 100;
+
+
+
+        ?>
+
+    </div>
+
+</body>
+</html>
